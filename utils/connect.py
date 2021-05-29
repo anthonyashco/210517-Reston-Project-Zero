cb25502d4a2b27f7587c239bdb398b3f318425f0
Anthony Co <anthonyashco@gmail.com>
2021-05-28 21:40:58 -0700

    Modify to check connection type from yml.

--- utils/connect.py
+++ utils/connect.py
@@ -5,4 +5,5 @@ import yaml
 class Connection():
     with open("settings.yml", "r") as f:
         settings = yaml.safe_load(f)
-    conn = connect(**settings["postgres"])
+        postgres = settings["postgres"]
+    conn = connect(**postgres[postgres["type"]])
