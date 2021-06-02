7108ebacca191a0f1c04dc3e105232d8a20ac1bc
Anthony Co <anthonyashco@gmail.com>
2021-06-02 08:49:32 -0700

    Add routes.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@
 import logging
 
 from flask import Flask
+from routes import routes
 
 app = Flask(__name__)
-
 logging.basicConfig(
     filename="log.log",
     level=logging.DEBUG,
     format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s")
+routes.create_routes(app)
 
 if __name__ == "__main__":
     app.run()
